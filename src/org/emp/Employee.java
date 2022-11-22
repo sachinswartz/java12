de2094febb102d2f2503a2074b1682f07568398f
sachinswartz <sachinswart2@gmail.com>
2022-11-22 20:00:52 +0530

    code added

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,14 @@
+package org.emp;
+
+public class Employee {
+	public void emp() {
+		System.out.println("emp name:gokul");
+
+	}
+	public static void main(String[] args) {
+		Employee a = new Employee();
+		a.emp();
+		
+	}
+
+}
